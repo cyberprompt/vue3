a7fb6ff1a1c89aa42f870e937343e23706bc8b9a
Ed Murphy <cyberprompt@gmail.com>
2022-02-11 12:22:17 -0500

    view/hide basic functionality working

--- js/lifecyclehooks.js
+++ js/lifecyclehooks.js
@@ -1,12 +1,13 @@
 const VCA = Vue.createApp({					// "options object"
     data: ()=> {							// data is a FUNCTION which RETURNS an OBJECT
         return{
-
+            isVisible: false
         }									// end return data 
     },										// end data
     methods: {								// methods object holds any functions you want to use
-
-
+        toggleBox: function(){
+            this.isVisible = !this.isVisible
+        }
     },                                      // end methods object			 
     mounted: function(){
         this.$nextTick(()=>{
@@ -14,3 +15,9 @@ const VCA = Vue.createApp({					// "options object"
         });
     }
 });	
+
+VCA.component('test-box', {
+    template: `
+        <div class="test-box"></div>
+    `
+});
