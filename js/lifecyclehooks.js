2d693497ab82bad98a91aee94432b0fbc528ccc0
Ed Murphy <cyberprompt@gmail.com>
2022-02-11 12:43:40 -0500

    all hook state logs added

--- js/lifecyclehooks.js
+++ js/lifecyclehooks.js
@@ -9,15 +9,52 @@ const VCA = Vue.createApp({					// "options object"
             this.isVisible = !this.isVisible
         }
     },                                      // end methods object			 
-    mounted: function(){
-        this.$nextTick(()=>{
-            // code to only run on complete view render
-        });
+    beforeCreate(){
+        console.log("VCA before create");
+    },
+    created(){
+        console.log("VCA created");
+    },
+    beforeMount(){
+        console.log("VCA before mount");
+    },
+    mounted(){
+        console.log("VCA mounted");
+    },
+    updated(){
+        console.log("VCA updated");
+    },
+    beforeUnmount(){
+        console.log("VCA before unmount");
+    },
+    unmounted(){
+        console.log("VCA unmounted");
     }
 });	
 
 VCA.component('test-box', {
     template: `
         <div class="test-box"></div>
-    `
+    `,    // Hooks defined below
+    beforeCreate(){
+        console.log("COMP before create");
+    },
+    created(){
+        console.log("COMP created");
+    },
+    beforeMount(){
+        console.log("COMP before mount");
+    },
+    mounted(){
+        console.log("COMP mounted");
+    },
+    updated(){
+        console.log("COMP updated");
+    }, 
+    beforeUnmount(){ 
+        console.log("COMP before unmount");
+    }, 
+    unmounted(){ 
+        console.log("COMP unmounted");
+    }
 });
